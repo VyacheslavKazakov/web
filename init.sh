d3d4a7d6b0c2e6ff228bc43bc7d466cffdcde6e8
VyacheslavKazakov <vyachka@gmail.com>
2019-01-09 22:52:54 +0500

    g 1.4

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart test
 sudo /etc/init.d/mysql start
