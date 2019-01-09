c0317b48061165bf39bc6afc7125c61987175c08
VyacheslavKazakov <vyachka@gmail.com>
2019-01-09 22:25:52 +0500

    gu 1.1

--- hello.py
+++ hello.py
@@ -5,3 +5,6 @@ def hello(environ, start_response):
     args = environ['QUERY_STRING'].split('&')
     start_response('200 OK', [('Content-Type', 'text/plain')])
     return [print(i) for i in args]
+
+bind = '0.0.0.0:8080'
+pythonpath = "/home/box/web/"
