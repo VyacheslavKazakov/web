c0d73ce52f1506d8ed6780a96387879faf4a29a4
VyacheslavKazakov <vyachka@gmail.com>
2019-01-09 23:04:03 +0500

    nginx default config removing

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 sudo rm /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/gunicorn.d/test
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
