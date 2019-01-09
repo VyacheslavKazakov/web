98ed03fcfdaef0458742603b24e4434f18309d88
VyacheslavKazakov <vyachka@gmail.com>
2019-01-09 22:16:13 +0500

    gunicorn 1.0

---
+++ hello.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+def hello(environ, start_response):
+    args = environ['QUERY_STRING'].split('&')
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [print(i) for i in args]
