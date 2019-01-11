9dd0b76c204ec4f3a8729c4939df89a6105c1e39
VyacheslavKazakov <vyachka@gmail.com>
2019-01-11 20:53:14 +0500

    dj 1.3

--- init.sh
+++ init.sh
@@ -2,9 +2,13 @@
 
 sudo rm /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/gunicorn.d/test
+sudo rm /etc/gunicorn.d/gunicorn-django.conf
 sudo rm /etc/nginx/sites-enabled/default
+
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/gunicorn-django.conf
 sudo /etc/init.d/gunicorn restart test
+sudo /etc/init.d/gunicorn restart gunicorn-django.conf
 sudo /etc/init.d/mysql start
