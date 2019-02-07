1ed591797a9d4599f550451321c0296f025393ad
VyacheslavKazakov <vyachka@gmail.com>
2019-02-07 22:38:20 +0500

    2.3 1.1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,21 +2,13 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-class qUser(User):
-
-    def __unicode__(self):
-        return self.username
-
-    class Meta:
-        db_table = 'users'
-
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
     rating = models.IntegerField(default = 0)
-    author = models.ForeignKey(qUser,null=False, on_delete=models.SET_NULL)
-    likes = models.ManyToManyField(qUser, related_name='question_likes_user')
+    author = models.ForeignKey(User,null=False, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name='question_likes_user')
 
     def __unicode__(self):
         return self.title
@@ -32,7 +24,7 @@ class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
     question = models.ForeignKey(Question,null=False, on_delete=models.CASCADE)
-    author = models.ForeignKey(qUser,null=False, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User,null=False, on_delete=models.SET_NULL)
 
     def __unicode__(self):
         return self.text
--- init.sh
+++ init.sh
@@ -15,5 +15,5 @@ sudo /etc/init.d/mysql start
 
 ###
 sudo mysql -uroot -e "CREATE DATABASE IF NOT EXISTS stepik DEFAULT CHARACTER SET = utf8"
-sudo mysql -uroot -e "CREATE USER djanga@localhost auth_option: { IDENTIFIED BY 'dj_pass' }"
-sudo mysql -uroot -e "GRANT ALL ON stepik.* TO djanga@localhost"
+sudo mysql -uroot -e "CREATE USER 'djanga' IDENTIFIED BY 'dj_pass'"
+sudo mysql -uroot -e "GRANT ALL ON stepik.* TO 'djanga'"
