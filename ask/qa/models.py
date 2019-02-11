1adb6bdf62fc6ec3e303aa53d0181b74d0c9d212
VyacheslavKazakov <vyachka@gmail.com>
2019-02-11 21:17:41 +0500

    2.3

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,6 +2,14 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
@@ -10,7 +18,7 @@ class Question(models.Model):
     author = models.ForeignKey(User,null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name='question_likes_user')
     objects = QuestionManager()
-    
+
     def __unicode__(self):
         return self.title
 
@@ -32,10 +40,3 @@ class Answer(models.Model):
 
     class Meta:
         db_table = 'answers'
-
-class QuestionManager(models.Manager):
-    def new(self):
-        return self.order_by('-added_at')
-
-    def popular(self):
-        return self.order_by('-rating')
