192aca5345b1117892fbca44ec3f8d3980cb7778
VyacheslavKazakov <vyachka@gmail.com>
2019-02-11 20:48:41 +0500

    2.3 2.0

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stepik',
+        'USER': 'djanga',
+        'PASSWORD': 'dj_pass',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
