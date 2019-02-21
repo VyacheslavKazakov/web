842bc0dcb2dc54ef5b798f45af9de0b2f1fa2142
VyacheslavKazakov <vyachka@gmail.com>
2019-02-21 22:27:28 +0500

    2.5 add 1.8

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -30,7 +30,7 @@ def home(request):
     questions = Question.objects.new()
     paginator, page = paginate(request, questions)
     # paginator.baseurl = '/?page='
-    paginator.baseurl = reverse('home', kwargs={'page': page})
+    paginator.baseurl = reverse('home', kwargs={'page': ''})
     return render(request, 'base.html', {'questions': page.object_list,
                                          'paginator': paginator,
                                          'page': page,})
@@ -40,7 +40,7 @@ def popular(request):
     questions = Question.objects.popular()
     paginator, page = paginate(request, questions)
     # paginator.baseurl = '/popular/?page='
-    paginator.baseurl = reverse('popular', kwargs={'page': page})
+    paginator.baseurl = reverse('popular', kwargs={'page': ''})
     return render(request, 'base.html', {'questions': page.object_list,
                                          'paginator': paginator,
                                          'page': page,})
