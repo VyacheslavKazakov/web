177e6bd98595ea15864fcb63fc4bb49e2860d888
VyacheslavKazakov <vyachka@gmail.com>
2019-03-01 20:04:19 +0500

    2.6 1.1

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_GET
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.core.paginator import Paginator
 from django.core.urlresolvers import reverse
 from qa.models import Question, Answer
